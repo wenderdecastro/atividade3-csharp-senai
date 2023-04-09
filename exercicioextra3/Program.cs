0f8ca866cdabff861bd215dab959da7bb8380f44
Wender <wendermaiac@gmail.com>
2023-04-09 18:50:54 -0300

    Atividades finalizadas

--- exercicioextra3/Program.cs
+++ exercicioextra3/Program.cs
@@ -1,8 +1,15 @@
-﻿for (var pessoas = 0; pessoas <= 10; pessoas++)
+﻿int numHomens = 0;
+int numMulheres = 0;
+bool sexoValido = false;
+int idadeTotalHomens = 0;
+int idadeTotalMulheres = 0;
+int pessoas = 0;
+
+for (pessoas = 0; pessoas < 10; pessoas++)
 {
     do
     {
-        Console.WriteLine($" Qual o seu sexo? Pressione 1 para homem e 2 para mulher. \r \n");
+        Console.WriteLine($" Qual o seu sexo? Pressione 1 para homem e 2 para mulher.");
 
         ConsoleKeyInfo inputSexo = Console.ReadKey();
 
@@ -20,32 +27,39 @@
         {
             case ConsoleKey.D1:
 
-                inputValido = false;
-
-                Console.WriteLine($"Você selecionou a opção homem.");
+                Console.WriteLine($"\r \nVocê selecionou a opção homem.");
 
                 numHomens++;
 
-                Console.WriteLine($"Qual a sua idade?");
-                int idade = int.Parse(Console.ReadLine());
-                
+                Console.WriteLine($"\r \nQual a sua idade?");
+                int idadeHomem = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\r \nQual o seu peso?");
+                float pesoHomens = int.Parse(Console.ReadLine());
+
+                idadeTotalHomens += idadeHomem;
 
                 break;
             case ConsoleKey.D2:
 
-                inputValido = false;
-
-                Console.WriteLine($"Você selecionou a opção mulher.");
+                Console.WriteLine($"\r \nVocê selecionou a opção mulher.");
 
                 numMulheres++;
+
+                Console.WriteLine($"\r \nQual a sua idade?");
+                int idadeMulher = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\r \nQual o seu peso?");
+                int pesoMulheres = int.Parse(Console.ReadLine());
+
+                idadeTotalMulheres += idadeMulher;
 
                 break;
         }
     } while (sexoValido == false);
-
-
-
-
-
-
 }
+
+Console.WriteLine($"O numero total de mulheres é de {numMulheres}");
+Console.WriteLine($"O numero total de homens é de {numHomens}");
+Console.WriteLine($"A média de idade das mulheres é igual a {Math.Round((float)idadeTotalMulheres / numMulheres, 2)}");
+Console.WriteLine($"A média de idade dos homens é igual a {Math.Round((float)idadeTotalHomens / numHomens, 2)}");
