1b7635e4b8a8efd9d4f2e83b3c269c18d8a706f7
Wender <wendermaiac@gmail.com>
2023-04-10 09:24:55 -0300

    Exericios finalizados

--- exercicioextra2/Program.cs
+++ exercicioextra2/Program.cs
@@ -1,20 +1,8 @@
 ﻿for (var num = 1; num <= 10; num++)
 {
-
-    Console.WriteLine(@$"A tabuada do numero {num} é:
-    
-    {num} . 1 = {num * 1}
-    {num} . 2 = {num * 2}
-    {num} . 3 = {num * 3}
-    {num} . 4 = {num * 4}
-    {num} . 5 = {num * 5}
-    {num} . 6 = {num * 6}
-    {num} . 7 = {num * 7}
-    {num} . 8 = {num * 8}
-    {num} . 9 = {num * 9}
-    {num} . 10 = {num * 10}
-
-    ");
+    Console.WriteLine(@$"A tabuada do numero {num} é: ");
+    for (var fator = 1; fator <= 10; fator++)
+    {
+        Console.WriteLine($"{num} . {fator} = {num * fator}");
+    }
 }
-
-Console.WriteLine("Hello, World!");
