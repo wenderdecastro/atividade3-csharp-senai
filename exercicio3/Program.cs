183eb89b713447c5908268b8445339423f4f037b
Wender <wendermaiac@gmail.com>
2023-04-10 09:27:08 -0300

    Exercicios concluidos

--- exercicio3/Program.cs
+++ exercicio3/Program.cs
@@ -16,16 +16,12 @@ do
 
 } while (valorValido == false);
 
-Console.WriteLine(@$"A tabuada do numero {num} é:
-{num} . 1 = {num * 1}
-{num} . 2 = {num * 2}
-{num} . 3 = {num * 3}
-{num} . 4 = {num * 4}
-{num} . 5 = {num * 5}
-{num} . 6 = {num * 6}
-{num} . 7 = {num * 7}
-{num} . 8 = {num * 8}
-{num} . 9 = {num * 9}
-{num} . 10 = {num * 10}
-");
+int fator;
+
+Console.WriteLine(@$"A tabuada do numero {num} é:");
+
+for (fator = 1; fator <= 10; fator++)
+{
+    Console.WriteLine($"{num} . {fator} = {num * fator}");
+}
 
